c512306ce7d29d94c83698d194b0f9f6f2bfdf81
Мария Мария <macbook.maria@MacBook-Pro-Maria.local>
2022-12-07 00:11:07 +0300

    Fixed commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mipt-projects
+
+Telegram-бот
+============
+
+Для работы бота установите модуль telegram-bot-python:
+
+
+.. code-block:: bash
+
+	user@computer$ pip3 install python-telegram-bot
+
+
+После этого перейдите в директорию tg-bot и создайте файл token.txt. В него поместите токен бота. После этого запустите бота
+
+
+.. code-block:: bash
+
+	user@computer:mipt-projects$ cd tg-bot
+	user@computer:tg-bot$ python3 main.py
+
+
+
--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,89 +1,182 @@
+#!/usr/bin/python3
+
 import config
 import telegram
 import json
 from random import randint
 
-TOKEN = config.token
-bot = telegram.bot.Bot(TOKEN)
+try:
+    token_file = open("token.txt", "r")
+    token = token_file.readlines()[0]
+    token_file.close()
+except FileNotFoundError:
+    print("ERROR! Failed to open token.txt")
+    print("Tip: create token.txt and put your bot's token in it")
+    exit()
 
 
+# Сессия создаётся для каждого, кто пишет что-то боту.
+class Session:
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+        self.testing = False
+        self.test_id = None
+        self.correct_ans = None
+        
+    def begin_test(self, test_id, correct_ans):
+        self.testing = True
+        self.test_id = test_id
+        self.correct_ans = correct_ans
+    
+    def end_test(self):
+        self.testing = False
+    
+    def check_ans(self, ans):
+        return str(self.correct_ans) == str(ans)
+
+
+# Получает номер последнего обновления
 def get_last_update_id(updates):
-    """Возвращает ID последнего апдейта"""
-    id_list = list()  # пустой список ID апдейтов
-    for update in updates:  # для каждого апдейта
-        id_list.append(update["update_id"])  # заносим в список его ID
-    return max(id_list)  # возвращаем последний
+    id_list = list()
+    for update in updates:
+        id_list.append(update["update_id"])
+    if len(id_list) == 0:
+        return 0
+    else:
+        return max(id_list)
 
-markup = telegram.ReplyKeyboardMarkup(
-    keyboard=['Как пользоваться']
-)
-recognize = {
-    'Как пользоваться': [bot.sendMessage, "Hello \n I can test your skill of recognizing nmr spectra \U0001F604"
-                                          " \n Firstly, a spectrum of proton magnetic resonance and four variants of molecules will be sent to you. You are to choose a suitable picture and type its number.\n If your answer is wrong, I'll send the correct answer with explanation."],
- #   'Stop': [exit, None]
-}
 
-def send_test(bot, tests_dict, chat_id):
+# Очищает обновления, полученные, когда бот был выключен.
+def clear_updates(bot):
+    last_update_id = get_last_update_id(bot.getUpdates())
+    #bot.getUpdates(last_update_id)
+    return last_update_id + 1
+
+
+# Находит сессию в списке по номеру чата. Создаёт новую сессию, если не находит.
+def get_session(chat_id, sessions):
+    for session in sessions:
+        if session.chat_id == chat_id:
+            return session
+    sessions.append(Session(chat_id))
+    return sessions[-1]
+
+
+# Обрабатывает сообщение
+def handle_message(message, sessions):
+    message_text = message['text']
+    chat_id = message['chat']['id']
+    session = get_session(chat_id, sessions)
+    
+    if session.testing:                                     # Если идёт тестирование
+        check_ans(session, message_text)                    # Проверить ответ
+        
+    elif message_text == "/start":                          # Если отправили /start
+        bot.sendMessage(chat_id, config.text_greeting)      # Отправить приветствие
+        
+    elif message_text == "/test":                           # Если отправили /test
+        send_test(session)                                  # Начать тестирование
+    
+    else:                                                   # Если команда не распознана
+        bot.sendMessage(chat_id, config.text_confused)      # Отправить сообщение с предупреждением
+
+
+# Загружает тесты из файла в tests_dict
+def load_tests():
+    global tests_dict
+    file = open("test.json", "r")
+    tests_dict = json.load(file)
+    file.close()
+
+
+# Тестирует пользователя
+def send_test(session):
     tests = tests_dict['tests']
     test_id = randint(0, len(tests) - 1)
     test = tests[test_id]
-    bot.sendPhoto(chat_id, test['img'])
-    correct_ans = test['correct_ans']
-    incorrect_ans = test['incorrect_ans']
+    
+    correct_ans = test["correct_ans"]
+    incorrect_ans = test["incorrect_ans"]
     all_ans = incorrect_ans + [correct_ans]
+    
+    bot.sendMessage(session.chat_id, config.text_testing)
+    try:
+        bot.sendPhoto(session.chat_id, test["img"])
+    except telegram.error.BadRequest:
+        print("Send test error!", test["img"])
+        bot.sendMessage(session.chat_id, config.text_img_error)
+        return
+        
     correct_ans_num = -1
     for i in range(len(all_ans)):
         ans_id = randint(0, len(all_ans) - 1)
-        bot.sendMessage(chat_id, str(i + 1) + '. ')
-        bot.sendPhoto(chat_id, all_ans[ans_id])
+        try:
+            bot.sendPhoto(session.chat_id, all_ans[ans_id], caption=str(i + 1))
+        except telegram.error.BadRequest:
+            print("Send test error!", all_ans[ans_id])
+            bot.sendMessage(session.chat_id, config.text_img_error)
+            return
         if all_ans[ans_id] == correct_ans:
             correct_ans_num = i + 1
         all_ans.pop(ans_id)
 
-    return test_id, correct_ans_num
+    session.begin_test(test_id, correct_ans_num)
 
 
-def check_ans(bot, tests_dict, chat_id, test_id, correct_ans_num, player_ans_num):
+# Проверяет ответ пользователя
+def check_ans(session, user_ans):
     tests = tests_dict['tests']
-    test = tests[test_id]
-    if str(player_ans_num) == str(correct_ans_num):
-        bot.sendMessage(chat_id, "Correct!!! \U0001F601")
+    test = tests[session.test_id]
+    
+    if session.check_ans(user_ans):
+        bot.sendMessage(session.chat_id, config.text_correct)
     else:
-        bot.sendMessage(chat_id, "Unfortunately the answer is wrong \U0001F601")
-        #        bot.sendMessage(chat_id, "The correct answer is " + str(correct_ans_num))
-        #        bot.sendMessage(chat_id, "Here is some theory to help you")
-        bot.sendPhoto(chat_id, test['help_img'])
+        bot.sendMessage(session.chat_id, config.text_wrong % (session.correct_ans))
+        try:
+            bot.sendPhoto(session.chat_id, test["help_img"])
+        except telegram.error.BadRequest:
+            print("Send help error!", test["help_img"])
+            bot.sendMessage(session.chat_id, config.text_help_error)
+            
+    session.end_test()
 
 
+# Запускается при запуске программы
 def main():
-    last_update_id = None
-    file = open("test.json", "r")
-    tests_dict = json.load(file)
-    file.close()
-    testing = False
+    print("===STARTING BOT===")
+
+    last_update_id = clear_updates(bot)
+    sessions = list()
     while True:
         updates = bot.getUpdates(last_update_id, timeout=100)
         if len(updates) > 0:
             last_update_id = get_last_update_id(updates) + 1
-            for update in updates:  # сообщения могут приходить быстро, быстрее, чем работает код
-                last_message = update["message"]  # взяли из него сообщение
-                if last_message :
-                    print(last_message)
-                    last_message_text = last_message['text']  # из сообщения - текст
-                    last_chat_id = last_message['chat']['id']  # и идентификатор чата
-                    if recognize.get(last_message_text):
-                        function = recognize[last_message_text][0]  # функция, которую мы вызываем -
-                        # нулевой элемент массива - значения по ключу
-#                        if last_message_text == 'Stop':
-#                            exit()
-                        arg = recognize[last_message_text][1]  # аргумент - его первый элемент
-                        function(last_chat_id, arg)
-                    if (not testing) and last_message_text == "Test me":
-                        testing = True
-                        test_id, correct_ans_num = send_test(bot, tests_dict, last_chat_id)
-                    elif testing:
-                        check_ans(bot, tests_dict, last_chat_id, test_id, correct_ans_num, last_message_text)
-                        testing = False
+            for update in updates:
+                last_message = update["message"]
+                if not last_message:
+                    continue
+                # print(last_message)
+                handle_message(last_message, sessions)
 
 
+# Бот
+global bot
+bot = telegram.bot.Bot(token)
+
+# Словарь тестов
+global tests_dict
+load_tests()
+
+# Запуск бота
 main()
+
+"""
+markup = telegram.ReplyKeyboardMarkup(
+    keyboard=['/start']
+)
+recognize = {
+    '/start': [bot.sendMessage, "Hello \n I can test your skill of recognizing nmr spectra \U0001F604"
+                                          " \n Firstly, a spectrum of proton magnetic resonance and four variants of molecules will be sent to you. You are to choose a suitable picture and type its number.\n If your answer is wrong, I'll send the correct answer with explanation."],
+ #   'Stop': [exit, None]
+}
+"""
